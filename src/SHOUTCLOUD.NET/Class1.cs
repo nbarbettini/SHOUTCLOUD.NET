3ade487ce373679557d46137169e5d146c95b9d6
Nate Barbettini <nathanaelb@gmail.com>
2016-04-10 14:20:43 -0700

    Initial project

---
+++ src/SHOUTCLOUD.NET/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoutcloud.NET
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
